25ecf95702f02a6f179665f96525d87a0a971f98
Joshua J. Bouw <jjb@unicity-labs.com>
2026-03-25 03:30:51 +0400

    chore: bump astrid to 0.5.1 (#4)
    Bump formula to v0.5.1 with updated SHA256 hashes from the release
    artifacts.

--- Formula/astrid.rb
+++ Formula/astrid.rb
@@ -1,26 +1,26 @@
 class Astrid < Formula
   desc "Microkernel for AI agents where agents write their own harness"
   homepage "https://github.com/unicity-astrid/astrid"
-  version "0.5.0"
+  version "0.5.1"
   license any_of: ["MIT", "Apache-2.0"]
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/unicity-astrid/astrid/releases/download/v#{version}/astrid-#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "89168c4e515729b1c9c93611b9bd176b2de021920b5c04f8dc96dad3b95fd8c4"
+      sha256 "97f694be3b39721befe90e1087e696b4997db1a0076a14731a688a865b527f06"
     else
       url "https://github.com/unicity-astrid/astrid/releases/download/v#{version}/astrid-#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "89aa49569e2ab685b6dcdb6ee1c27147a28042a01f911fb7e03f00e9b84b69ec"
+      sha256 "9f5dff707cf1136f8b1fa3360c496988e8455816b385ae6ffd4da5339a7d0abc"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/unicity-astrid/astrid/releases/download/v#{version}/astrid-#{version}-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "fdb467d09852c00baad157968e3380b93e182545570f46a6c140a4c2500034d4"
+      sha256 "75cc98c990f44c37e1c53e1c0debd244ad1b0e8b1316ada23112e11b18dc2ab8"
     else
       url "https://github.com/unicity-astrid/astrid/releases/download/v#{version}/astrid-#{version}-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "8160ba93b1c07952df5f1a4e525307d68016028f4591096b48c59f4fa7eb25b7"
+      sha256 "c9fe6c733aca58c3cd559e2bf566710a379f5742bc0c91e57e526b00005f40b0"
     end
   end
 
